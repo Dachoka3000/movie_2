6cdd1069ea548d5f149c6d944ce230a90497123a
Machoka Daisy <machokadaisy25@gmail.com>
2022-10-04 04:22:50 +0300

    add total salary method

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,5 +1,13 @@
 class Actor < ActiveRecord::Base
     has_many :roles
     has_many :movies , through: :roles
+
+    def total_salary
+        total = 0
+        self.roles.each do |role|
+            total += role.salary
+        end
+        total
+    end
     
 end
