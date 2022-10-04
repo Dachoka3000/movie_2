d95382ce731357fac67bd3667e8e3d3b1ca3146b
Machoka Daisy <machokadaisy25@gmail.com>
2022-10-04 04:47:04 +0300

    add cast role an all credits methods

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,17 @@
 class Movie < ActiveRecord::Base
     has_many :roles
     has_many :actors, through: :roles
+
+    def cast_role(actor:,character_name:,salary:)
+        new_role = Role.create(salary:salary, character_name:character_name,actor_id:actor.id,movie_id:self.id)
+    end
+
+    def all_credits
+        self.roles.map do |role|
+            "#{role.character_name}:Played by #{role.actor.name}"
+        end
+    end
+
+    def fire_actor(actor:)
+    end
 end
